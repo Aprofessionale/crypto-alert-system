6538157394875720b994c6729c45ef0e32d3e96e
Aprofessionale <fraxenq@gmail.com>
2025-04-16 00:13:34 +0000

    Test generating mock price

---
+++ cmd/collector/price_simulator.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"math/rand"
+	"time"
+)
+
+type PriceData struct {
+	Symbol string  `json:"symbol"`
+	Price  float64 `json:"price"`
+	Time   int64   `json:"time"`
+}
+
+func GenerateMockPrice() PriceData {
+	symbols := []string{"BTC", "ETH"}
+	symbol := symbols[rand.Intn(len(symbols))]
+
+	basePrice := map[string]float64{
+		"BTC": 60000,
+		"ETH": 3000,
+	}
+
+	price := basePrice[symbol] + rand.Float64()*1000 - 500 // +/- 500
+	return PriceData{
+		Symbol: symbol,
+		Price:  price,
+		Time:   time.Now().Unix(),
+	}
+}
---
+++ cmd/collector/price_simulator_test.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestGenerateMockPrice(t *testing.T) {
+	price := GenerateMockPrice()
+
+	assert.NotEmpty(t, price.Symbol)
+	assert.True(t, price.Price > 0)
+}
