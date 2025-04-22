0039bbdecf54beb6c1d7e7af1f9f831a187a131f
Aprofessionale <fraxenq@gmail.com>
2025-04-22 17:50:17 +0700

    test publisher_test.go

--- test/collector/publisher_test.go
+++ test/collector/publisher_test.go
@@ -3,16 +3,18 @@ package collector_test
 import (
 	"context"
 	"testing"
+
+	"github.com/aprofessionale/crypto-alert-system/cmd/collector"
 )
 
 func TestPublishPrice(t *testing.T) {
-	price := PriceData{
+	price := collector.PriceData{
 		Symbol: "BTC",
 		Price:  60200.12,
 		Time:   1713100000,
 	}
 
-	err := PublishPrice(context.Background(), price)
+	err := collector.PublishPrice(context.Background(), price)
 
 	if err != nil {
 		t.Errorf("failed to publish price: %v", err)
