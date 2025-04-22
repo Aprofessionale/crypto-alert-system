0e4e4999942a81fa12962d548efb29638eb09408
Aprofessionale <fraxenq@gmail.com>
2025-04-22 20:18:06 +0700

    test unit test processor

--- internal/processor/consumer.go
+++ internal/processor/consumer.go
@@ -10,6 +10,10 @@ import (
 	"github.com/segmentio/kafka-go"
 )
 
+type KafkaReader interface {
+	ReadMessage(ctx context.Context) (kafka.Message, error)
+}
+
 func ConsumePriceMessages(ctx context.Context) error {
 	reader := kafka.NewReader(kafka.ReaderConfig{
 		Brokers: []string{"localhost:9092"},
@@ -37,5 +41,23 @@ func ConsumePriceMessages(ctx context.Context) error {
 
 			fmt.Printf("Received price: %s - $%.2f\n", price.Symbol, price.Price)
 		}
+	}
+}
+
+func ConsumePrices(reader KafkaReader, out chan<- common.PriceData) error {
+	for {
+		msg, err := reader.ReadMessage(context.Background())
+		if err != nil {
+			return err
+		}
+
+		var price common.PriceData
+		if err := json.Unmarshal(msg.Value, &price); err != nil {
+			log.Printf("Failed to unmarshal: %v ", err)
+			continue
+		}
+
+		log.Printf("Consumed: %+v", price)
+		out <- price
 	}
 }
--- test/processor/consumer_test.go
+++ test/processor/consumer_test.go
@@ -5,6 +5,7 @@ import (
 	"testing"
 	"time"
 
+	"github.com/aprofessionale/crypto-alert-system/internal/common"
 	processor "github.com/aprofessionale/crypto-alert-system/internal/processor"
 	"github.com/segmentio/kafka-go"
 )
@@ -41,4 +42,47 @@ func TestConsumePriceMessage(t *testing.T) {
 	time.Sleep(2 * time.Second) // Allow time to consume
 
 	// You could add logs/assertions or mock the handler later
+}
+
+type MockKafkaReader struct {
+	messages []kafka.Message
+}
+
+func (m *MockKafkaReader) ReadMessage(ctx context.Context) (kafka.Message, error) {
+	if len(m.messages) == 0 {
+		<-ctx.Done()
+		return kafka.Message{}, ctx.Err()
+	}
+	msg := m.messages[0]
+	m.messages = m.messages[1:]
+	return msg, nil
+}
+
+func TestConsumePrices(t *testing.T) {
+	mockMsg := kafka.Message{
+		Key:   []byte("BTC"),
+		Value: []byte(`{"Symbol":"BTC","Price":61234.56,"Time":1713400000}`),
+	}
+
+	reader := &MockKafkaReader{
+		messages: []kafka.Message{mockMsg},
+	}
+
+	received := make(chan common.PriceData, 1)
+
+	go func() {
+		err := processor.ConsumePrices(reader, received)
+		if err != nil {
+			t.Errorf("Consumer prices failed: %v ", err)
+		}
+	}()
+
+	select {
+	case p := <-received:
+		if p.Symbol != "BTC" || p.Price != 61234.56 {
+			t.Errorf("Unexpected price data %+v", p)
+		}
+	case <-time.After(2 * time.Second):
+		t.Error("did not receive message.")
+	}
 }
