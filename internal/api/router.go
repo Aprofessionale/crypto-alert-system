59e5251aca38e533e6e3b2b6954badaf5a163609
Aprofessionale <fraxenq@gmail.com>
2025-04-25 14:51:50 +0700

    add routing to /healthz

--- internal/api/router.go
+++ internal/api/router.go
@@ -1,7 +1,16 @@
-package router
+package api
 
-import "fmt"
+import (
+	"net/http"
 
-func route() {
-	fmt.Print("This is router.go")
+	handlers "github.com/aprofessionale/crypto-alert-system/internal/api/handlers"
+)
+
+func NewStdLibRouter() *http.ServeMux {
+	mux := http.NewServeMux()
+
+	// Health check
+	mux.HandleFunc("GET /healthz", handlers.HealthCheckHandler)
+
+	return mux
 }
