0cc51e5695fe94a78791df7e207049091a186936
Aprofessionale <fraxenq@gmail.com>
2025-04-25 15:13:09 +0700

    config setup for server

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,7 +1,27 @@
 package config
 
-import "fmt"
+import (
+	"os"
+)
 
-func config() {
-	fmt.Print("This is config.go")
+type Config struct {
+	Port string
+}
+
+func LoadConfig() (*Config, error) {
+	port := getEnv("PORT", "8080")
+
+	cfg := &Config{
+		Port: port,
+	}
+
+	return cfg, nil
+}
+
+func getEnv(key, fallback string) string {
+	if value, exists := os.LookupEnv(key); exists {
+		return value
+	}
+
+	return fallback
 }
